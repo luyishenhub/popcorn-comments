f71d41d83964cd667395fd4c544af84ce71ca7a8
Cynthia <s38ren@uwaterloo.ca>
2021-01-16 12:37:04 -0500

    add scrolling

--- Chrome Extension/inject.js
+++ Chrome Extension/inject.js
@@ -8,15 +8,29 @@
     console.log(videos);
     
     let frame = videos.item(0);
-    console.log(frame);
+    console.log(frame.offsetWidth);
     
     // just place a div at top right
     var div = document.createElement("div");
-    div.style.position = "relative";
+    div.style.position = "absolute";
     div.style.top = 0;
     div.style.right = 0;
     div.style.color = "white";
     div.textContent = "Injected!";
 
     frame.appendChild(div);
+
+    // scrolling comment
+    var pos = 0 - div.offsetWidth;
+    var id = setInterval(move, 10);
+
+    function move() {
+        if (pos == frame.offsetWidth) {
+            frame.removeChild(div);
+            clearInterval(id);
+        } else {
+            pos++;
+            div.style.right = pos + 'px';
+        }
+    }
 })();
