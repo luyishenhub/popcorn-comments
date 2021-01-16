729b2a97bf37a373ad91f0ed26a0d0892310d371
Cynthia <s38ren@uwaterloo.ca>
2021-01-16 17:44:14 -0500

    fix http request & pause/play

--- Chrome Extension/inject.js
+++ Chrome Extension/inject.js
@@ -10,13 +10,16 @@
     let frame = videos.item(0);
     console.log(frame.offsetWidth);
 
-    // TODO: list of popcorn comments
-    var comments = ["comment1", "long comment2", "very long comment3", "very very very long comment4", "comment5", "comment6", "very very long comment7", "long comment8", "comment9", "long comment10"];
-    // var comments = [" ", " ", " ", " ", " ", " ", " ", " "];
+    var comments = [];
+    var layers = [];
+    var pos = [];
+    
+    var isPaused = false;
+
+    var id;
 
     getComments();
  
-
     // http request to fetch all comments
     function getVideoID(url){
         var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
@@ -31,13 +34,42 @@
 
         var xmlHttp = new XMLHttpRequest();
         xmlHttp.onreadystatechange = function() { 
-            // console.log(xmlHttp.status)
             if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+                console.log("success")
                 // callback
                 const obj = JSON.parse(xmlHttp.responseText);
                 console.log(obj.items);
                 comments = obj.items;
                 console.log(comments[0]);
+
+                // fill comments
+                for (var i = 0; i < 6; i++) {
+                    padding = 40 + 30*i;
+                    var popcorn = document.createElement("div");
+                    popcorn.style.position = "absolute";
+                    popcorn.style.paddingTop = padding.toString() + "px";
+                    popcorn.style.color = "white";
+                    popcorn.style.fontSize = "16px";
+                    popcorn.style.overflow = "hidden";
+                    popcorn.style.whiteSpace = "nowrap";
+                    updateComment(popcorn);
+            
+                    frame.appendChild(popcorn);
+                    layers.push(popcorn);
+            
+            
+                    console.log(popcorn.textContent);
+            
+            
+                    // scrolling comment
+                    var init = 0 - popcorn.offsetWidth;
+            
+                    pos.push(init);
+            
+                }
+
+                var id = setInterval(move, 10);
+
             }
                 
         }
@@ -48,93 +80,59 @@
             console.log("http request fired");
         }
     }
-
     
-    
-    // layers of popcorn comments
-    var layers = [];
-    var pos = [];
+ 
+     // observe YouTube video pause/play
+     var pauseObserver = new MutationObserver(function(mutations) {
+         mutations.forEach(function(mutation) {
+             if (mutation.type === "attributes" && mutation.attributeName === "aria-label") {
+                 console.log(mutation);
+                 isPaused = !isPaused;
+             }
+         });
+     });
+     
+     pauseObserver.observe(document.getElementsByClassName("ytp-play-button").item(0), {
+         attributes: true
+       });
+ 
+ 
+     function move() {
+ 
+         // TODO: stop/scroll scrolling upon pause/play
+         if (layers.length == 0) {
+             clearInterval(id);
+             pauseObserver.disconnect();
+         }
+         if (!isPaused) {
+             for (var i = 0; i < layers.length; i++) {
+                 if (pos[i] == frame.offsetWidth) {
+         
+                     if (comments.length > 0) {
+                         updateComment(layers[i]);
+                         pos[i] = 0 - layers[i].offsetWidth;
+     
+                         console.log(layers[i].textContent);
+                     } else {
+                         layers.splice(i, 1);
+                         pos.splice(i, 1);
+                     }
+                     
+                 } else {
+                     pos[i]++;
+                     layers[i].style.right = pos[i] + 'px';
+                         
+                 }
+     
+             }
+ 
+         }
+     }
+     
+     function updateComment(elem) {
+         elem.textContent = comments.shift();
+         elem.style.right = (-elem.offsetWidth).toString() + "px";
+     }
 
-    
-    for (var i = 0; i < 6; i++) {
-        padding = 40 + 30*i;
-        var popcorn = document.createElement("div");
-        popcorn.style.position = "absolute";
-        popcorn.style.paddingTop = padding.toString() + "px";
-        popcorn.style.color = "white";
-        popcorn.style.fontSize = "16px";
-        popcorn.style.overflow = "hidden";
-        popcorn.style.whiteSpace = "nowrap";
-        updateComment(popcorn);
-
-        frame.appendChild(popcorn);
-        layers.push(popcorn);
-
-
-        console.log(popcorn.textContent);
-
-
-        // scrolling comment
-        var init = 0 - popcorn.offsetWidth;
-
-        pos.push(init);
-
-    }
-    
-    var isPaused = false;
-
-    // observe YouTube video pause/play
-    var pauseObserver = new MutationObserver(function(mutations) {
-        mutations.forEach(function(mutation) {
-            console.log(mutation);
-            isPaused = !isPaused;
-        });
-    });
-    
-    // pauseObserver.observe(document.getElementsByClassName("ytp-play-button").item(0), {
-    //     attributes: true
-    //   });
-
-
-    var id = setInterval(move, 10);
-
-    function move() {
-
-        // TODO: stop/scroll scrolling upon pause/play
-        if (layers.length == 0) {
-            clearInterval(id);
-            // pauseObserver.disconnect();
-        }
-        if (!isPaused) {
-            for (var i = 0; i < layers.length; i++) {
-                if (pos[i] == frame.offsetWidth) {
-        
-                    if (comments.length > 0) {
-                        updateComment(layers[i]);
-                        pos[i] = 0 - layers[i].offsetWidth;
-    
-                        console.log(layers[i].textContent);
-                    } else {
-                        layers.splice(i, 1);
-                        pos.splice(i, 1);
-                    }
-                    
-                } else {
-                    pos[i]++;
-                    layers[i].style.right = pos[i] + 'px';
-                        
-                }
-    
-            }
-
-        }
-    }
-    
-    function updateComment(elem) {
-        elem.textContent = comments.shift();
-        elem.style.right = (-elem.offsetWidth).toString() + "px";
-    }
-
-
-
+   
 })();
