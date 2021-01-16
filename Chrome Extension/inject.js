c5325c06c264b14cf5d5bdb4e900d7961cce1130
Cynthia <s38ren@uwaterloo.ca>
2021-01-16 15:31:39 -0500

    scrolling & update rows of comment

--- Chrome Extension/inject.js
+++ Chrome Extension/inject.js
@@ -1,5 +1,5 @@
 (function () {
-    alert("inserted");
+    // alert("inserted");
 
     // change video style
 
@@ -9,28 +9,74 @@
     
     let frame = videos.item(0);
     console.log(frame.offsetWidth);
+
+    // TODO: list of popcorn comments
+    var comments = ["comment1", "long comment2", "very long comment3", "very very very long comment4", "comment5", "comment6", "very very long comment7", "long comment8", "comment9", "long comment10"];
+
     
-    // just place a div at top right
-    var div = document.createElement("div");
-    div.style.position = "absolute";
-    div.style.top = 0;
-    div.style.right = 0;
-    div.style.color = "white";
-    div.textContent = "Injected!";
+    
+    // layers of popcorn comments
+    var layers = [];
+    var pos = [];
+    
+    for (var i = 0; i < 6; i++) {
+        padding = 40 + 30*i;
+        var popcorn = document.createElement("div");
+        popcorn.style.position = "absolute";
+        popcorn.style.paddingTop = padding.toString() + "px";
+        popcorn.style.color = "white";
+        popcorn.style.fontSize = "16px";
+        popcorn.style.overflow = "hidden";
+        popcorn.style.whiteSpace = "nowrap";
+        updateComment(popcorn);
 
-    frame.appendChild(div);
+        frame.appendChild(popcorn);
+        layers.push(popcorn);
 
-    // scrolling comment
-    var pos = 0 - div.offsetWidth;
+
+        console.log(popcorn.textContent);
+
+
+        // scrolling comment
+        var init = 0 - popcorn.offsetWidth;
+
+        pos.push(init);
+
+    }
+
     var id = setInterval(move, 10);
 
     function move() {
-        if (pos == frame.offsetWidth) {
-            frame.removeChild(div);
-            clearInterval(id);
-        } else {
-            pos++;
-            div.style.right = pos + 'px';
+
+        // TODO: stop/scroll scrolling upon pause/play
+        for (var i = 0; i < layers.length; i++) {
+            if (pos[i] == frame.offsetWidth) {
+    
+                // if (pos[index] == 200) {
+                clearInterval(id);
+                updateComment(layers[i]);
+                pos[i] = 0 - layers[i].offsetWidth;
+
+
+                console.log(layers[i].textContent);
+
+                id = setInterval(move, 10);
+        
+        
+            } else {
+                pos[i]++;
+                layers[i].style.right = pos[i] + 'px';
+                    
+            }
+
         }
     }
+    
+    function updateComment(elem) {
+        elem.textContent = comments.shift();
+        elem.style.right = (-elem.offsetWidth).toString() + "px";
+    }
+
+
+
 })();
