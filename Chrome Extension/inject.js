29183d931d0651fd6c6fb7dca9fe9ad198630b14
Cynthia <s38ren@uwaterloo.ca>
2021-01-16 11:49:40 -0500

    modify-inject

--- Chrome Extension/inject.js
+++ Chrome Extension/inject.js
@@ -3,18 +3,20 @@
 
     // change video style
 
-    var videos = document.getElementsByClassName("html5-video-container");
-    console.log("here!");
-    console.log(videos);
-
-    let frame = videos.item(0);
+    // var videos = document.getElementsByClassName("html5-video-container");
+    // console.log("here!");
+    // console.log(videos);
+    
+    // let frame = videos.item(0);
+    var frame = document.getElementById('player-theater-container');
     console.log(frame);
-
+    
     // just place a div at top right
     var div = document.createElement("div");
-    div.style.position = "fixed";
+    div.style.position = "relative";
     div.style.top = 0;
     div.style.right = 0;
+    div.style.color = "white";
     div.textContent = "Injected!";
 
     frame.appendChild(div);
